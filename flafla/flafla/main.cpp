c2a94975702ef3c34087a9264a242918321801d9
kaikai <q521kai@outlook.com>
2017-07-24 22:06:42 +0800

    2

--- flafla/flafla/main.cpp
+++ flafla/flafla/main.cpp
@@ -1,7 +1,62 @@
-#include <stdlib.h>
+#include <stdio.h>
+#include <windows.h>
 
 int main()
+//{
+//	int map();
+//	system("pause");
+//	return 0;
+//}
+//
+//int map(void)
 {
+	int x, y, arr[10][10] =
+	{
+		{ 0,1,1,1,1,1,1,1,1,1 },
+		{ 1,1,1,1,1,1,1,1,1,1 },
+		{ 1,1,1,1,1,1,1,1,1,1 },
+		{ 1,1,1,1,1,1,1,1,1,1 },
+		{ 1,1,1,1,1,1,1,1,1,1 },
+		{ 1,1,1,1,1,1,1,1,1,1 },
+		{ 1,1,1,1,1,1,1,1,1,1 },
+		{ 1,1,1,1,1,1,1,1,1,1 },
+		{ 1,1,1,1,1,1,1,1,1,1 },
+		{ 1,1,1,1,1,1,1,1,1,1 },
+	};
+	for (x = 0; x < 10; x++)
+	{
+		for (y = 0; y < 10; y++)
+		{
+			if (arr[x][y] == 0)
+			{
+				printf("¡õ");
+			}
+			else
+			{
+				printf("¡ö");
+			}
+		}
+		printf("\n");
+	}
+	//system("pause");
+	getchar();
 
+	return 0;
+}
+
+int main01()
+{
+	int a = 0;
+	while (1)
+	{
+		printf("This is NULL");
+		printf("\n");
+		a += 1;
+		if (a == 10)
+		{
+			break;
+		}
+	}
+	system("pause");
 	return 0;
 }
